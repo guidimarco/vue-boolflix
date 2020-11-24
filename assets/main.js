42c77e48eb0cb5a8567127eafdeee796c9b09f8e
Marco Guidi <mark.88g@gmail.com>
2020-11-24 15:43:34 +0100

    HTML-CSS-JS aggiunte bandiere

--- assets/main.js
+++ assets/main.js
@@ -10,8 +10,8 @@ var app = new Vue({ // VUE INSTANCE
             axios
                 .get("https://api.themoviedb.org/3/search/movie", { params: {
                         api_key: "f14b811e77f424ab83b5ac2e25d349b8", // api key
-                        query: this.userSearch,
-                        language: "it"
+                        query: this.userSearch, // user's search --> see data
+                        language: "it",
                     }
                 }).then( (filmsList) => {
                     this.films = filmsList.data.results;
@@ -29,8 +29,12 @@ var app = new Vue({ // VUE INSTANCE
             // new-vote function: from 0 to 5
             return newVote = Math.round(thisVote / 2);
         },
-    },
+        setAltFlag: function(event) {
+            let srcNoFlag = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/No_flag.svg/1280px-No_flag.svg.png";
 
+            event.target.src = srcNoFlag;
+        },
+    },
     mounted: function() {
         this.searchFilm();
     },
