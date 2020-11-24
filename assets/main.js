5a2f295c716e766c17e80db3c7eff5eeb86afa4e
Marco Guidi <mark.88g@gmail.com>
2020-11-24 13:01:32 +0100

    HTML struttura base JS richiesta axios

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,22 @@
 var app = new Vue({ // VUE INSTANCE
     el: "#root",
     data: {
-
+        userSearch: "",
     },
     methods: {
+        searchFilm: function() {
+            let currentSearch = this.userSearch;
 
-    },
-    mounted() {
-        
+            // axios request --> films and tv series
+            axios
+                .get("https://api.themoviedb.org/3/search/movie?api_key=f14b811e77f424ab83b5ac2e25d349b8&query=batman")
+                .then( (filmsList) => {
+                    console.log(filmsList);
+                })
+            ;
+
+            // reset the search
+            this.userSearch = "";
+        },
     },
 });
