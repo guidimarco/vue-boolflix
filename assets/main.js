4e391697f956192b5256ecd7ce5332f2af2b4992
Marco Guidi <mark.88g@gmail.com>
2020-11-26 12:57:58 +0100

    correzioni codice: cambiato parametri funzioni, fatte 2 chiamate api

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
 // API VAR
 const myApiKey = "f14b811e77f424ab83b5ac2e25d349b8"; // my api key
-const filmApiUrl = "https://api.themoviedb.org/3/"; // api url for films and series
+const apiUrl = "https://api.themoviedb.org/3/"; // api url for films and series
 const posterUrl = "https://image.tmdb.org/t/p/"; // api url for posters and backdrop
 const posterSize = "w300"; // backdrop size (width 300px)
 
@@ -9,6 +9,7 @@ var app = new Vue({ // VUE INSTANCE
     data: {
         myFlags: "de en es fr it ja pt", // all my flags
         altFlag: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/No_flag.svg/1280px-No_flag.svg.png", // alternative flag
+        maxStars: 5, // maximum rate vote
         inputClass: "",
         searchText: "",
         searchMsg: "",
@@ -25,56 +26,60 @@ var app = new Vue({ // VUE INSTANCE
             // check: get api only if thisSearch != 0
             if (thisSearch) {
                 // local VAR
-                let currentList; // list of films from server
+                let currentPar = {
+                    params: {
+                        api_key: myApiKey,
+                        query: thisSearch, // user's search --> see data
+                        language: "it",
+                    }
+                } // current search parameters
+
+                // set the DOM
                 this.isLoading = true; // START the loading-render
                 this.searchMsg = thisSearch; // change the msg
+                this.toggleSearchBar(); // close the search bar
 
-                // close the search bar
-                this.toggleSearchBar();
+                axios // axios request --> films
+                    .get(apiUrl + "search/movie", currentPar)
+                    .then( (answer) => {
+                        this.films = this.films.concat(answer.data.results);
 
-                // axios request --> films and tv series
-                axios
-                    .get(filmApiUrl + "search/multi", { params: {
-                            api_key: myApiKey,
-                            query: thisSearch, // user's search --> see data
-                            language: "it",
-                        }
-                    }).then( (filmsResults) => {
-                        currentList = filmsResults.data.results;
+                        // set the DOM
+                        this.isLoading = false; // END the loading-render
+                    })
+                ;
 
-                        // filter the results: discard the person
-                        currentList = currentList.filter( (item) => {
-                            return item.media_type != "person";
-                        });
+                axios // axios request --> tv series
+                    .get(apiUrl + "search/tv", currentPar)
+                    .then( (answer) => {
+                        this.films = this.films.concat(answer.data.results);
 
-                        this.films = currentList; // push the list into the data
+                        // set the DOM
                         this.isLoading = false; // END the loading-render
                     })
                 ;
             } // END if: get films
         },
-        setCardBgr: function(filmIndex) {
-            // local var
-            let thisPath = app.films[filmIndex].backdrop_path; // this backdrop-path
+        setCardBgr: function(backdropPath) {
+            // backdropPath --> final part of url backdrop-poster
             let thisBgrStyle; // this background style (url or white)
 
-            if (thisPath != null) {
+            if (backdropPath != null) {
                 // it's not null --> assembly url and return it
-                thisBgrStyle = "background-image: url(\"" + posterUrl + posterSize + thisPath + "\";";
+                thisBgrStyle = "background-image: url(\"" + posterUrl + posterSize + backdropPath + "\";";
             } else {
                 // it's null --> background white
                 thisBgrStyle = "background-color: white;";
             }
             return thisBgrStyle;
         },
-        setFlag: function(filmIndex) {
-            // local var
-            let thisLang = app.films[filmIndex].original_language; // this original lang
-            let flagUrl; // flag src
+        setFlag: function(filmFlag) {
+            // filmFlag --> current film language (same as flag)
+            let flagUrl; // final flag src --> TO RETURN
 
-            if (this.myFlags.includes(thisLang)) {
+            if (this.myFlags.includes(filmFlag)) {
                 // set local src
-                flagUrl = "assets/flags/" + thisLang + ".png";
+                flagUrl = "assets/flags/" + filmFlag + ".png";
             } else {
                 // set alternative url
                 flagUrl = this.altFlag;
@@ -90,13 +95,12 @@ var app = new Vue({ // VUE INSTANCE
 
             return thisType == "movie";
         },
-        nStars: function(filmIndex) {
-            // local var
-            let thisVote = app.films[filmIndex].vote_average; // old vote 0-10
-            let nStars; // number of stars
+        setStars: function(filmVote) {
+            // filmVote: from 1 to 10
+            let numberOfStars; // number of stars --> TO RETURN
 
             // new-vote function: from 0 to 5
-            return nStars = Math.round(thisVote / 2);
+            return numberOfStars = Math.ceil(filmVote * this.maxStars / 10);
         },
         toggleSearchBar: function() {
             if (!this.inputClass) {
